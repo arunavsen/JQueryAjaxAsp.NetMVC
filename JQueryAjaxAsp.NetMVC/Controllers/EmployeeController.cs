35ac695d109e1eda0fc4c420b2db71fca4f7bd4b
Arunav Sen <arunavsen96@gmail.com>
2021-08-04 20:47:57 +0600

    Add ViewAll action and view

--- JQueryAjaxAsp.NetMVC/Controllers/EmployeeController.cs
+++ JQueryAjaxAsp.NetMVC/Controllers/EmployeeController.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using JQueryAjaxAsp.NetMVC.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
@@ -8,10 +9,26 @@ namespace JQueryAjaxAsp.NetMVC.Controllers
 {
     public class EmployeeController : Controller
     {
+        private readonly ApplicationDbContext _context;
+        public EmployeeController()
+        {
+            var context = new ApplicationDbContext();
+            _context = context;
+        }
         // GET: Employee
         public ActionResult Index()
         {
             return View();
+        }
+
+        public ActionResult ViewAll()
+        {
+            return View(GetAllEmployees());
+        }
+
+        public IEnumerable<Employee> GetAllEmployees()
+        {
+            return _context.Employees.ToList();
         }
     }
 }
