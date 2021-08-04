45416c562525a2dc0dcfd4ccc0d803ca509f22c0
Arunav Sen <arunavsen96@gmail.com>
2021-08-04 19:44:47 +0600

    Add ApplicationDbContext class and Update Database

---
+++ JQueryAjaxAsp.NetMVC/Migrations/Configuration.cs
@@ -0,0 +1,23 @@
+﻿namespace JQueryAjaxAsp.NetMVC.Migrations
+{
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<JQueryAjaxAsp.NetMVC.Models.ApplicationDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = false;
+        }
+
+        protected override void Seed(JQueryAjaxAsp.NetMVC.Models.ApplicationDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+        }
+    }
+}
---
+++ JQueryAjaxAsp.NetMVC/Models/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace JQueryAjaxAsp.NetMVC.Models
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext() : base("DefaultConnection")
+        {
+
+        }
+    }
+}
