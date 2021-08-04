fe98319f5387daff4f1b00574cbe39ac5fc354ff
Arunav Sen <arunavsen96@gmail.com>
2021-08-04 19:26:31 +0600

    Add Employee Model

---
+++ JQueryAjaxAsp.NetMVC/Models/Employee.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace JQueryAjaxAsp.NetMVC.Models
+{
+    public class Employee
+    {
+        [Key]
+        public int EmployeeId { get; set; }
+        public string Name { get; set; }
+        public string Position { get; set; }
+        public string Office { get; set; }
+        public int? Salary { get; set; }
+        public string ImagePath { get; set; }
+    }
+}
