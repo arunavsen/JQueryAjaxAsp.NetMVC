a04e64e88877967fe6df1137ca6f3a118aac92b1
Arunav Sen <arunavsen96@gmail.com>
2021-08-08 03:42:29 +0600

    AddOrEdit action to add employees

--- JQueryAjaxAsp.NetMVC/Controllers/EmployeeController.cs
+++ JQueryAjaxAsp.NetMVC/Controllers/EmployeeController.cs
@@ -1,6 +1,7 @@
 ﻿using JQueryAjaxAsp.NetMVC.Models;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -31,6 +32,7 @@ namespace JQueryAjaxAsp.NetMVC.Controllers
             return _context.Employees.ToList();
         }
 
+        [HttpGet]
         public ActionResult AddOrEdit(int id = 0)
         {
             Employee emp = new Employee();
@@ -38,9 +40,33 @@ namespace JQueryAjaxAsp.NetMVC.Controllers
         }
 
         [HttpPost]
-        public ActionResult AddOrEdit()
+        public ActionResult AddOrEdit(Employee emp)
         {
-            return View();
+            try
+            {
+                if (emp.ImageUpload != null)
+                {
+                    string fileName = Path.GetFileNameWithoutExtension(emp.ImageUpload.FileName);
+
+                    string extension = Path.GetExtension(emp.ImageUpload.FileName);
+
+                    fileName = fileName + DateTime.Now.ToString("yymmssfff") + extension;
+
+                    emp.ImagePath = "~/AppFiles/Images/" + fileName;
+
+                    emp.ImageUpload.SaveAs(Path.Combine(Server.MapPath("~/AppFiles/Images/"), fileName));
+                }
+
+                _context.Employees.Add(emp);
+                _context.SaveChanges();
+
+                return Json(new { success = true, html = GlobalClass.RenderRazorViewToString(this, "ViewAll", GetAllEmployees()), message = "Submitted Successfully" }, JsonRequestBehavior.AllowGet);
+            }
+            catch (Exception ex)
+            {
+
+                return Json(new { success = false, message = ex.Message }, JsonRequestBehavior.AllowGet);
+            }
         }
     }
 }
--- JQueryAjaxAsp.NetMVC/Scripts/Scripts.js
+++ JQueryAjaxAsp.NetMVC/Scripts/Scripts.js
@@ -7,3 +7,41 @@
         reader.readAsDataURL(imageUploader.files[0]);
     }
 }
+
+function jQueryAjaxPost(form) {
+    $.validator.unobtrusive.parse(form);
+    if ($(form).valid()) {
+
+        var ajaxConfig = {
+            type: 'POST',
+            url: form.action,
+            data: new FormData(form),
+            success: function (response) {
+                if (response.success) {
+                    $("#firstTab").html(response);
+                    refreshAddNewTab($(form).attr('data-resetUrl'), true);
+                } else {
+                    //Error message
+                }
+            }
+        };
+
+        if ($(form).attr('enctype') == "multipart/form-data") {
+            ajaxConfig["contentType"] = false;
+            ajaxConfig["processData"] = false;
+        }
+        $.ajax(ajaxConfig);
+    }
+    return false;
+}
+
+function refreshAddNewTab(restUrl, showViewTab) {
+    $.ajax({
+        type: 'GET',
+        url: restUrl,
+        success: function (response) {
+            $("#secondTab").html(response);
+            $('ul.nav.nav-tabs a:eq(0)').tab('show');
+        }
+    });
+}
